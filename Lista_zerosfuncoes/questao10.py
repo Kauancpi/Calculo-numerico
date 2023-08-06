bf1e3f3e1897f522aa6c522638ee45aad6e52514
Kauancpi <kauancpi@gmail.com>
2023-08-06 18:08:01 -0300

    Questao 10 pronta

---
+++ Lista_zerosfuncoes/questao10.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def pythag(a,b):
+    p=max(np.fabs(a),np.fabs(b))
+    q=min(np.fabs(a),np.fabs(b))
+    while(np.fabs(q)>0.00000000001):
+        r=(q/p)**2
+        s=r/(4+r)
+        p=p+2*s*p
+        q=s*q
+    return(p)
+
+def norma_euclidiana(x):
+    s=0
+    for i in range(0,len(x)):
+        s=pythag(s,x[i])
+    return(s)
+
+
+print("teste com 3,4:",pythag(3,4))
+print("teste com -5,12:",pythag(-5,12))
+print("teste com 7,24:",pythag(7,24))
+print("teste norma euclidiana 3,4,5,6,7,8:",norma_euclidiana([3,4,5,6,7,8]))
