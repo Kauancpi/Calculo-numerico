41aa4b45de289ade7150e0247c2f390153f2689f
Kauancpi <kauancpi@gmail.com>
2023-08-08 15:19:52 -0300

    questao 2 pronta

--- Lista_zerosfuncoes/questao2.py
+++ Lista_zerosfuncoes/questao2.py
@@ -27,7 +27,7 @@ print("Erro aproximado: "+ str(erro_percentual(zf.pontofixo(g,phi,3,0.01,3),zf.p
 print("A raiz encontrada pelo metodo de newton:",zf.metodo_newton(g,der,3,0.01,3))
 print("Erro aproximado: "+ str(erro_percentual(zf.metodo_newton(g,der,3,0.01,3),zf.pontofixo(g,phi,3,0.01,100)))+"%")
 print("A raiz encontrada pelo metodo das secantes:", zf.metodo_secante(g,4,3,0.01,3))
-print("Erro aproximado: "+ str(erro_percentual(zf.metodo_secante(g,4,3,0.01,3),zf.pontofixo(g,phi,3,0.01,100)))+"%")
+print("Erro aproximado: "+ str(erro_percentual(zf.metodo_secante(g,3,4,0.01,3),zf.pontofixo(g,phi,3,0.01,100)))+"%")
 plt.show()
 
 
