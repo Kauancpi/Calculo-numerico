cac98d203fd272d31a9ed26d5c4bfbcc6ac0ffc6
Kauancpi <kauancpi@gmail.com>
2023-08-09 09:16:01 -0300

    deixei bonito

--- Funcoes_importantes/zerosfuncoes.py
+++ Funcoes_importantes/zerosfuncoes.py
@@ -4,53 +4,70 @@ import numpy as np
 #metodo da bisseccao
 #f= funcao, a = menor valor, b = maior valor, dx= tolerancia, iteracoes = iteracoes
 def bissec(f,a,b, dx, iteracoes):
+    
     x=0
     media = (b+a)/2
+    
     while ((f(media)>dx or f(media)<-dx) and x<iteracoes ):
+        
         if f(media)*f(a)<0:
             b=media
             media = (media+a)/2
             x+=1
             continue
+        
         elif f(media)*f(b)<0:
             a=media
             media = (b+media)/2
             x+=1
             continue
+    
     else:
         return(media)
     
 #Metodo da Falsa posicao
 #f=funcao, a = menor valor, b= maior valor, dx= tolerancia, iteracoes = iteracoes
 def fakeposition(f,a,b,dx,iteracoes):
+    
     if f(a)*f(b) > 0:
         print("Não dá pra usar esse método")
         return(False)
+    
     x=0
     mediap = (a*fabs(f(b))+b*fabs(f(a)))/(fabs(f(a))+fabs(f(b)))
+    
     while (f(mediap)>(dx) or f(mediap)<-(dx)) and x<iteracoes:
+        
         if f(mediap)*f(a)<0:
+            
             b=mediap
             mediap = (a*fabs(f(mediap))+mediap*fabs(f(a)))/(fabs(f(a))+fabs(f(mediap)))
             x+=1
             continue
+        
         elif f(mediap)*f(b)<0:
+            
             a=mediap
             mediap = (b*fabs(f(mediap))+mediap*fabs(f(b)))/(fabs(f(b))+fabs(f(mediap)))
             x+=1
             continue
+    
     else:
         return(mediap) 
     
 # Metodo do ponto fixo
 #f=funcao, phi = funcao de iteracao, inicial=chute inicial, dx = tolerancia, iteracoes = iteracoes
 def pontofixo(f,phi,inicial,dx,iteracoes):
+    
     x_k=[inicial]
     x=0
+    
     while((fabs(f(x_k[-1]))>dx)and x<iteracoes):
+        
         x_next=phi(x_k[-1])
         x_k.append(x_next)
         x+=1
+    
     return(x_k[-1])
 
 # Funcao de iteracao utilizada no metodo de newton
@@ -60,12 +77,15 @@ def phinewton(x,f,der):
 # metodo de Newton
 #f= funcao, der = derivada da funcao, inicial = chute inicial, dx = tolerancia, iteracoes= iteracoes
 def metodo_newton(f,der,inicial,dx,iteracoes):
+    
     x_k=[inicial]
     x=0
+    
     while((fabs(f(x_k[-1]))>dx) and x<iteracoes):
         x_next=phinewton(x_k[-1],f,der)
         x_k.append(x_next)
         x+=1
+    
     return(x_k[-1])
     
 # Funcao de iteracao metodo da secante
@@ -73,12 +93,15 @@ def iteracao_secante(f,x0,x1):
     return(x1-f(x1)*(x1-x0)/(f(x1)-f(x0)))
 
 # Metodo da secante
-#f= funcao, x0,x1=intervalo, x0<x1, dx=tolerancia, iteracoes=iteracoes
+#f= funcao, x0=menor valor intervalo, x1= maior valor intervalo, dx=tolerancia, iteracoes=iteracoes
 def metodo_secante(f,x0,x1,dx,iteracoes):
+    
     x_k=[x0,x1]
     x=0
+    
     while((fabs(f(x_k[-1]))>dx)and x<iteracoes):
         x_next=iteracao_secante(f,x_k[-2],x_k[-1])
         x_k.append(x_next)
         x+=1
+    
     return(x_k[-1])
--- Lista_zerosfuncoes/questao1.py
+++ Lista_zerosfuncoes/questao1.py
@@ -11,8 +11,8 @@ def f(x):
 
     
 
-x=np.linspace(0.5,2.5,100)
-plt.plot(x,f(x),"b")
+x=np.linspace(0.5,2.6,100)
+plt.plot(x,f(x),"b--")
 print("A raiz encontrada pelo metodo da bissecção:",zf.bissec(f,0.5,2,0.01,10))
 print("A raiz encontrada pelo metodo da falsa posicao:",zf.fakeposition(f,0.5,2.5,0.01,10))
 plt.show()
